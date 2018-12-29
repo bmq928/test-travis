61f38918869248578ce869251eb5ccf11d0346f3
bmq92 <bmq928@gmail.com>
2018-12-29 23:18:54 +0700

    cd

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const { exec } = require('child_process')
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log('success')
-  res.send('success')
+  exec('cd ./app-inside && git pull origin master', (err) => {
+    console.log('success')
+    res.send('success')
+  })
+  
 })
 
 app.listen(3000, () => console.log('start'))
